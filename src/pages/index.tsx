9f64bbf5fc5c794b0edf1386423e120f74ad0e87
Alexander Santiago <arsantiagolopez@gmail.com>
2024-02-19 21:42:22 -0600

    Lets make history

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-[80px]">
+      The best way to Bitcoin.
+    </main>
+  );
+};
+
+export default HomePage;
